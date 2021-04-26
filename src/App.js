3db4a6e75a9900521a970040dccec97833a3b70a
SesH.iO <sesh.io1022@gmail.com>
2021-04-26 14:10:15 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { GlobalStyle } from './global.styles';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Fragment>
+			<GlobalStyle />
+			<h1>This World Shall Know Pain</h1>
+		</Fragment>
+	);
 }
 
 export default App;
